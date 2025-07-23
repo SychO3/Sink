3b686bd2984091ac6bc9f0d9777f47648766ad4f
SychO <vbdd@msn.com>
2025-07-23 12:18:37 +0800

    fix: update social media links and remove unused components in the footer

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,7 +3,7 @@ export default defineAppConfig({
   email: 'sink.cool@miantiao.me',
   github: 'https://github.com/ccbikai/sink',
   twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
+  telegram: 'https://https://t.me/ShadowistBot',
   mastodon: 'https://sink.cool/mastodon',
   blog: 'https://sink.cool/blog',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
